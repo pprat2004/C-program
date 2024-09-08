78d6801d9bb9af5f1491e3a0789368efb75e4283
pratham <prathamramesh2004007@gmail.com>
2024-09-08 12:32:29 +0530

    Update pratham.c

--- pratham.c
+++ pratham.c
@@ -4,7 +4,7 @@ int main ()
    int x, res;
    x = 12;
    res = x / 2;
-   printf("The the quotient of thw two numbers is : %d", res);
+   printf("The  quotient of  two numbers is : %d", res);
 
     return 0;
 }
